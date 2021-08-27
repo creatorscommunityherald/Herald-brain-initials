2aaea5fe9d5911cb3237cbb06b616df7e13ba50f
Sandip Thapa <sandipthapa386@gmail.com>
2021-08-27 09:42:12 +0545

    linter setup

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -6,9 +6,9 @@ export default function Header() {
                 <div className="row">
                     <div className="col-md-10 col-lg-8 mx-auto position-relative">
                         <div className="site-heading" style={{ height: "691px" }}>
-                            <h2>if we put all together what we know there is much we don't know about.</h2>
+                            <h2>if we put all together what we know there is much we do not know about.</h2>
                             <h2 className="text-end" style={{ color: "rgb(141,76,181)", margin: "2em" }}>we are supposed to explore, we are supposed to built, and we are supposed to inspire</h2>
-                            <h2 className="text-center" style={{ color: "rgb(255,255,255)" }}>let's work on that, together.</h2>
+                            <h2 className="text-center" style={{ color: "rgb(255,255,255)" }}>let&aposs work on that, together.</h2>
                             <h2 className="text-center" style={{ color: "rgb(141,76,181)" }}></h2>
                         </div>
                     </div>
