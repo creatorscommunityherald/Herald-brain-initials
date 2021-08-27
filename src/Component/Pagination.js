bc53edc825d714250bbdaafd952e56cb89599913
Sandip Thapa <sandipthapa386@gmail.com>
2021-08-27 09:42:39 +0545

    update after linter setup

--- src/Component/Pagination.js
+++ src/Component/Pagination.js
@@ -1,17 +1,17 @@
 export default function Pagination(){ 
  return ( 
 
- <nav  className="Paginationn">
-                            <ul className="pagination">
-                                <li className="page-item"><a className="page-link" href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
-                                <li className="page-item"><a className="page-link" href="#">1</a></li>
-                                <li className="page-item"><a className="page-link" href="#">2</a></li>
-                                <li className="page-item"><a className="page-link" href="#">3</a></li>
-                                <li className="page-item"><a className="page-link" href="#">4</a></li>
-                                <li className="page-item"><a className="page-link" href="#">5</a></li>
-                                <li className="page-item"><a className="page-link" href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
-                            </ul>
-                        </nav>
+ <nav className="Paginationn">
+    <ul className="pagination">
+        <li className="page-item"><a className="page-link" href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+        <li className="page-item"><a className="page-link" href="#">1</a></li>
+        <li className="page-item"><a className="page-link" href="#">2</a></li>
+        <li className="page-item"><a className="page-link" href="#">3</a></li>
+        <li className="page-item"><a className="page-link" href="#">4</a></li>
+        <li className="page-item"><a className="page-link" href="#">5</a></li>
+        <li className="page-item"><a className="page-link" href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+    </ul>
+</nav>
  
  ) 
 }
